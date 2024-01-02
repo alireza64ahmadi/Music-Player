1f62cba0b9fb9b229d7d4393f859f5b8c9c7f39f
alireza <121108490+alireza64ahmadi@users.noreply.github.com>
2024-01-03 02:50:29 +0330

    change some style of the page
    -change some style of the page

--- script.js
+++ script.js
@@ -65,45 +65,8 @@ function loadTrack(track_index) {
   updateTimer = setInterval(setUpdate, 1000);
 
   curr_track.addEventListener("ended", nextTrack);
-  random_bg_color();
 }
 
-function random_bg_color() {
-  let hex = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-  ];
-  let a;
-
-  function populate(a) {
-    for (let i = 0; i < 6; i++) {
-      let x = Math.round(Math.random() * 14);
-      let y = hex[x];
-      a += y;
-    }
-    return a;
-  }
-  let Color1 = populate("#");
-  let Color2 = populate("#");
-  var angle = "to right";
-
-  let gradient =
-    "linear-gradient(" + angle + "," + Color1 + ", " + Color2 + ")";
-  document.body.style.background = gradient;
-}
 function reset() {
   curr_time.textContent = "00:00";
   total_duration.textContent = "00:00";
